f4edd60a8a5873d938ac466f95683bd44bae1548
Radislav Berkovich <radiber@amazon.com>
2022-01-09 23:53:12 +0200

    prettier

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,35 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './Components/Header';
-import Search from './Components/Search';
-import { useState } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import Header from "./Components/Header";
+import Search from "./Components/Search";
 
-const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY
-
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 const App = () => {
-  const [word, setWord] = useState('');
+  const [word, setWord] = useState("");
 
   const handleSearchSubmit = (e) => {
     e.preventDefault(e);
+
     //console.log("handleSearchSubmit", word);
-    fetch(`https://api.unsplash.com/photos/random?query=${word}&client_id=${UNSPLASH_KEY}`)
+    const url = `https://api.unsplash.com/photos/random?query=${word}&client_id=${UNSPLASH_KEY}`;
+    fetch(url)
       .then((res) => res.json())
       .then((data) => {
-        console.log("data",data);
+        console.log("data", data);
+        setWord("");
       })
       .catch((err) => {
-        console.log('err to fetch', err);
-      })
-  }
+        console.log("err to fetch", err);
+      });
+  };
 
   //console.log("unsplash key", UNSPLASH_KEY);
   return (
-    <div >
+    <div>
       <Header title="Images gallery header" />
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
-
     </div>
   );
-}
+};
 
 export default App;
--- frontend/src/Components/Search.js
+++ frontend/src/Components/Search.js
@@ -1,40 +1,38 @@
-import React from 'react';
-import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import React from "react";
 
-
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
 const Search = ({ word, setWord, handleSubmit }) => {
+  const onWordChange = (e) => {
+    //console.log("onWordChange",e.target.value, e);
+    setWord(e.target.value);
+  };
 
-    const onWordChange = (e) => {
-        //console.log("onWordChange",e.target.value, e);
-        setWord(e.target.value)
-    }
-
-
-    return (
-        <Container className="mt-4 ">
-            <Row className="justify-content-center">
-                <Col xs={12} md={8} lg={6}>
-                    <Form onSubmit={handleSubmit}>
-                        <Form.Group as={Row}>
-
-                            <Col xs={9}>
-                                <Form.Control
-                                    value={word}
-                                    type="text"
-                                    onChange={e => onWordChange(e)}
-                                    placeholder="Search for images ..."
-                                />
-                            </Col>
-                            <Col sm={2}>
-                                <Button variant="primary" type="submit" >Search</Button>
-                            </Col>
-                        </Form.Group>
-                    </Form>
-                </Col>
-            </Row>
-        </Container>
-    )
+  return (
+    <Container className="mt-4 ">
+      <Row className="justify-content-center">
+        <Col xs={12} md={8} lg={6}>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group as={Row}>
+              <Col xs={9}>
+                <Form.Control
+                  value={word}
+                  type="text"
+                  onChange={(e) => onWordChange(e)}
+                  placeholder="Search for images ..."
+                />
+              </Col>
+              <Col sm={2}>
+                <Button variant="primary" type="submit">
+                  Search
+                </Button>
+              </Col>
+            </Form.Group>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Search;
