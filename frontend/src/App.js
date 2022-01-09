3dee9ca58808ebd14708bd61ace58b52121b5297
Radislav Berkovich <radiber@amazon.com>
2022-01-09 22:21:13 +0200

    first api call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,25 @@ import Header from './Components/Header';
 import Search from './Components/Search';
 import { useState } from 'react';
 
-
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY
 
 const App = () => {
   const [word, setWord] = useState('');
 
   const handleSearchSubmit = (e) => {
     e.preventDefault(e);
-    console.log("handleSearchSubmit", word);
+    //console.log("handleSearchSubmit", word);
+    fetch(`https://api.unsplash.com/photos/random?query=${word}&client_id=${UNSPLASH_KEY}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("data",data);
+      })
+      .catch((err) => {
+        console.log('err to fetch', err);
+      })
   }
 
+  //console.log("unsplash key", UNSPLASH_KEY);
   return (
     <div >
       <Header title="Images gallery header" />
