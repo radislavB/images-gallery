a12e24ad01d2c5832a390c28db7a989ac44ba94b
Radislav Berkovich <radiber@amazon.com>
2022-01-13 15:20:49 +0200

    add pymongo

--- api/main.py
+++ api/main.py
@@ -1,9 +1,11 @@
+from crypt import methods
 import os
+import json
 import requests
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 from flask_cors import CORS
-from mongo_client import insert_test_document
+import mongo_client
 
 load_dotenv(dotenv_path="./.env.local")
 UNSPLASH_URL = "https://api.unsplash.com/photos/random"
@@ -18,23 +20,36 @@ app = Flask(__name__)
 CORS(app)
 app.config["DEBUG"] = DEBUG
 
-insert_test_document()
 
 @app.route("/")
 def root_message():
-    ''' welcome message '''
+    """welcome message"""
     return "Hello from API app"
 
 
 @app.route("/new_image")
 def new_image():
-    ''' get new random image data '''
+    """get new random image data"""
     word = request.args.get("query")
     params = {"query": word}
     headers = {"Authorization": "Client-ID " + UNSPLASH_KEY, "Accept-Version": "v1"}
     response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
     data = response.json()
     return data
+
+
+@app.route("/images", methods=["GET", "POST"])
+def images():
+    if request.method == "GET":
+        return jsonify(mongo_client.get_images())
+    elif request.method == "POST":
+        print("post images")
+        image = request.get_json()
+        image["_id"] = image["id"]
+        
+        result = mongo_client.save_images(image)
+        inserted_id = result.inserted_id
+        return {"inserted_id": inserted_id}
 
 
 if __name__ == "__main__":
--- api/mongo_client.py
+++ api/mongo_client.py
@@ -18,8 +18,23 @@ mongo_client = MongoClient(
     port=MONGO_PORT,
 )
 
+gallery = mongo_client.gallery
+images_collection = gallery.images
+
+
+def get_images():
+    ''' get images from mongo'''
+    images = images_collection.find({})
+    return [img for img in images]
+
+
+def save_images(image_json):
+    '''save images'''
+    return images_collection.insert_one(image_json)
+
 
 def insert_test_document():
+    """test method"""
     db = mongo_client.test
     test_collection = db.test_collection
     result = test_collection.insert_one({"name": "radi", "instructor": False})
